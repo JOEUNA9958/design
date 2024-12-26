e11ca5da9c91941e65c37c1bc734fdf9b031fd31
JOEUNA9958 <dmsdk0808@naver.com>
2024-12-26 17:59:22 +0900

    1226

--- animal/js/main.js
+++ animal/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     let device_status // 브라우저가 pc인지 mobile 상태인지 
     let window_w //브라우저의 넓이 
     let scrolling // 브라우저가 스크롤 된 값 
+    let tab_name //find 의 클릭한 tab의 이름
 
 
     /******************* visual 팝업 (시작) *********************/
@@ -70,8 +71,10 @@ $(document).ready(function(){
         /**스크롤을 내린 상태에서 마우스를 오버했다가 아웃하면 header에 클래스가 사라짐
         스크롤 된 값이 0이거나 0보다 작을때만 삭제 **/
         if(scrolling <= 0){
-            $(this).removeClass('fixed')
-            console.log('마우스를 header에서 내렸을때')
+            if($('header').hasClass('sch_open') == false){ // header에 sch_open에 없을때
+                $(this).removeClass('fixed')
+                console.log('경우의 수1')
+            }
         }
     })
 
@@ -81,16 +84,12 @@ $(document).ready(function(){
         // console.log(scrolling)
         if(scrolling > 0){ //스크롤을 내렸을때
             $('header').addClass('fixed')
-        }else{ //맨꼭대기
+        }else if(($('header').hasClass('sch_open') == false) && ($('header').hasClass('menu_pc') == false)){ //맨꼭대기
             /** 검색이 열려있는 상태에서는 class삭제 안함 --- header에 sch_open 클래스가 있으면 열린 상태 **/
-            if($('header').hasClass('sch_open') == false){
-                if($('header').hasClass('menu_pc') == false){ // 
-                    $('header').removeClass('fixed')
-                    console.log('위로 스크롤 했을때')
-                }
-            }
+            $('header').removeClass('fixed')
         }
     }
+   
     scroll_chk() //함수의 실행 - 로딩된 후 1번
     $(window).scroll(function(){
         scroll_chk() //함수의 실행 - 스크롤 될때마다 1번씩 실행 
@@ -124,13 +123,14 @@ $(document).ready(function(){
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin' , function(){
         if(device_status == 'pc'){ //pc 모드일때
             // 이전에 오버했던 메뉴의 over를 삭제하기 위해서 모든 li의 over 지웟다가 오버한 애만 추가 
-            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
-            $(this).addClass('over')
-            $('header').addClass('menu_pc')
-            $('header').addClass('fixed')
-            // console.log('오버다')
+            if($('header').hasClass('sch_open') == false){
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+                $(this).addClass('over')
+                $('header').addClass('menu_pc')
+                $('header').addClass('fixed')
+                // console.log('오버다')
+            }
         }
-
     })
     $('header .gnb .gnb_wrap ul.depth1').on('mouseleave' , function(){
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
@@ -145,4 +145,104 @@ $(document).ready(function(){
     })
     
     /********** pc 버전 메뉴 (종료) **********/
+    //* 모바일 버전 메뉴 열기 시작
+    /*header .gnb .gnb_open 를 클릭하면 header에 menu_mo 클래스 추가 
+      header .gnb .gnb_close 를 클릭하면 header에 menu_mo 클래스 삭제*/
+
+
+    // console.log('메뉴 열기 전 ')  
+    $('header .gnb .gnb_open').on('click' , function(){
+        // console.log('누름')
+        $('header ').addClass('menu_mo')
+        $('html, body').css({overflow : 'hidden', height : $(window).height()}).bind('scroll touchmove mousewheel', function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+    })
+    $('header .gnb .gnb_close').on('click' , function(){
+        $('header ').removeClass('menu_mo')
+        $('html, body').css({overflow : 'visible', height : 'auto'}).unbind('scroll touchmove mousewheel');
+    })// 모바일버전 메뉴열기 종료
+
+
+    // 모바일 2차 메뉴 열기 시작
+    /*
+    * li를 클릭할것이냐 a를 클릭할것이냐 -> a를 클릭
+    * --> 2차 메뉴가 있는 1차 메뉴는 하위메뉴를 여는 기능을 갖음 (자기 자신의 href는 작동안함)
+    * --> 2차 메뉴를 가지고 있는 1차 메뉴인지 아닌지 구분
+    * header .gnb .gnb_wrap ul.depth1 > li:has(ul.depth2) > a
+    * 1차 메뉴 li에 open 이라는 클래스를 추가 
+    *
+    * 1. 클릭한 메뉴만 열리고 다른 메뉴는 모두 닫기
+    * 2.이미 열려있는 메뉴를 다시 클릭하면 닫기
+    * 메뉴를 클릭했을때 이미 열린 메뉴 (나만닫기) 또는  닫혀 메뉴 (나머지 다 닫고, 나만 열기)
+    */ 
+    $('header .gnb .gnb_wrap ul.depth1 > li:has(ul.depth2) > a').on('click', function(e){
+        if( device_status == 'mobile'){ // 모바일 일때만 
+            e.preventDefault(); //href 를 작동안하게 막는 코드 
+        }
+        if($((this)).parent().hasClass('open') == false){
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
+            $(this).parent().addClass('open')
+        }else{
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
+        }
+    })
+
+    /***모바일 버전 2차 메뉴열기 종료*/
+
+    /* 찾습니다 (가족을)*/
+    const find_panel01_swiper = new Swiper('.find .panel01 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            641: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        // centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.find .tab_content .panel01 .btn_wrap .next',
+            prevEl: '.find .tab_content .panel01 .btn_wrap .prev',
+        },
+    });
+
+    const find_panel02_swiper = new Swiper('.find .panel02 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            641: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.find .tab_content .panel02 .btn_wrap .next',
+            prevEl: '.find .tab_content .panel02 .btn_wrap .prev',
+        },
+    });
+    /** 찾습니다. 가족을 탭 가능**
+     * 
+     * .find .tab_list ul li 버튼을 누른 후 하는 일
+     * 버튼을 누른 후 하는 일 
+     * li에 active 클래스 추가, li button 에 title="선택됨 입력
+     * li에 data-tab 의 값을 가져와서 tab_content .tab_panel 중에서 data-tab 이 같은 값인 요소
+     * 
+     * 
+     * **/
+
+    $('.find .tab_list ul li').on('click' , function(){
+        $('.find .tab_list ul li').removeClass('active')
+        $(this).addClass('active')
+        $('.find .tab_list ul li button').attr('title' , '')
+        $(this).find('button').attr('title', '선택됨')
+        tab_name = $(this).attr('data-tab')
+        console.log(tab_name)
+        $('.find .tab_content .tab_panel').removeClass('active')
+        $('.find .tab_content').find('[data-tab="'+tab_name+'"]').addClass('active')
+    })
+
+
+
 })
