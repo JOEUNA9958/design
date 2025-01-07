008dbc3a050dc7bb6388a54a3fbc8f57e47d60ff
JOEUNA9958 <dmsdk0808@naver.com>
2025-01-07 12:27:57 +0900

    20250107

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -107,7 +107,7 @@ $(document).ready(function(){
             }
         }
     })
-    /******** 언어 선택 닫기 *********/      
+    /******** 언어 선택 닫기 종료 *********/      
 
     /******** 모바일 2차 메뉴 열고 닫기 시작  *********
      * header .gnb .gnb_wrap ul.depth1 > li > a 를 클릭했을때
@@ -116,8 +116,7 @@ $(document).ready(function(){
      * >> 열려있으면 닫고, 닫혀있으면 (다른애들을 모두 닫고 나만 열기)
      * /
 
-    
-    /******** 모바일 2차 메뉴 열고 닫기  *********/
+    /****/
 
     $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
         if(device_status == 'mobile'){
@@ -136,7 +135,7 @@ $(document).ready(function(){
                 $(this).next().slideDown()
             }
         }
-    })
+    })/* 모바일 2차 메뉴 열고 닫기 종료*/ 
 
     /******** 모바일 메뉴 열기 닫기 시작 *
      * header .gnb .gnb_open >> 클릭하면 열리고 header에 menu_mo 클래스 추가 
@@ -150,8 +149,36 @@ $(document).ready(function(){
     })
     $('header .gnb .gnb_close').on('click', function(){
         $('header').removeClass('menu_mo')
-    })
+    }) /**모바일 메뉴 열기 닫기 종료 **/
 
+
+    /******top버튼을 클릭하면 상단으로 스크롤, 시작******/ 
+    $('footer .top button ').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0, 
+        }, 500) //500이 0.5초 , 1000이 1초 
+    })
+    /********family_site 시작 *****
+     * footer .family_site button 클릭했을때
+     * footer .family_site button에 open 클래스 추가
+     * button 에 title의 문구 변경 
+     * footer .family_site .list 열고 닫기 slideUp slideDown
+     * >>> 현재 버튼이 열려있는지 닫혀 있는지 구분 >> open클래스 존재 유무로 
+     * */
+    $('footer .family_site button').on('click', function(){
+        if($(this).parent().hasClass('open') == true){ //open 클래스가 있을때 (열렸을때 >> 닫는기능)
+            // console.log('open 클래스 있음')
+            $(this).parent().removeClass('open')
+            $(this).next().slideUp()
+            $(this).attr('title', '열기버튼')
+        }else{ //open 클래스가 없을을때 (닫혔을때 >> 여는기능)
+            // console.log('open 클래스 없음') 
+            $(this).parent().addClass('open')
+            $(this).next().slideDown()
+            $(this).attr('title', '닫기버튼')
+        }
+    })
+    
 })//$(document).ready
 
    
