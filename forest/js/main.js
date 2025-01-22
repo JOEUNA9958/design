f9604180224f0305d68d0f26e810c5f7781d5088
JOEUNA9958 <dmsdk0808@naver.com>
2025-01-22 18:01:12 +0900

    250122

--- forest/js/main.js
+++ forest/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         /* navigation 은 글자를 클릭할수가 업승ㅁ - 다른 기능을 사용 anchor ,menu*/
         
         lockAnchors: false, /* anchor를 사용 하려면 해제 */
-        menu: '.quick_nav', 
+        // menu: '.quick_nav', 
         anchors: ['main', 'tree', 'introduce', 'story'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
         autoScrolling:true, /* 한페이지씩 스크롤 */
@@ -22,38 +22,44 @@ $(document).ready(function(){
         scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
         afterLoad: function(origin, destination, direction, trigger){
-            if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-                console.log('3번째 슬라이드가 로딩 되었을때');
+            $('.quick_nav ul li').removeClass('active')
+            $('.quick_nav ul li').eq().addClass('active')
+            if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+                // console.log('Main');
+            }else if(destination.index == 1){
+                $('header').addClass('dark')
+                $('quick_link').addClass('dark')
+                // console.log('나무심기');
+            }else if(destination.index == 2){
+                $('header').addClass('dark')
+                // console.log('활동');
+            }else if(destination.index == 3){
+                // console.log('활동이야기');
             }
         },
-
         responsiveWidth: 1025, /* fullpage를 적용시키지 않을 모바일 사이즈 */
         responsiveHeight: 700,
     });//fullpage
 
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    let visual_name = ['생명의숲', '서울 마이트리', '고목나무 이야기', '#같이가치#매달기부']
+    // console.log(visual_name[3])
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싸는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
+            delay: 5000,
             disableOnInteraction: true,
         },
     
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
     
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            // type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
             renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
+                return '<div class="' + className + '"><button>' + visual_name[index] + "</button></div>";
             },
         },
         
-    
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.swiper-button-prev',  
-        },
-    
     });
 })
