45ecb12bd8af2b56b949e9fe7370c7f924688245
JOEUNA9958 <dmsdk0808@naver.com>
2025-02-13 18:00:48 +0900

    250213

--- Hayung/js/sub.js
+++ Hayung/js/sub.js
@@ -2,3 +2,13 @@
     sub.js
     sub 레이아웃 요소 (공통요소)에서 작동되는 스크립트
 */
+
+$(document).ready(function(){
+    $('.container .lnb .lnb_open').on('click', function(){
+        $('.container .lnb').addClass('open')
+    })
+    $('.container .lnb .lnb_close').on('click', function(){
+        $('.container .lnb').removeClass('open')
+    }) /**모바일 메뉴 열기 닫기 종료 **/
+})
+
